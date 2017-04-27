167283b8bdd386598e8cd48e37e714bf2bdbd9eb
renestl <renestl@gmail.com>
2017-04-26 19:21:55 -0500

    completed fetch and show badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,17 @@
 $(function() {
 
-  // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/renestl.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      response.courses.completed.forEach(function(element) {
+        $('#badges').append(`
+          <div class="course">
+            <h3>${element.title}</h3>
+            <img src="${element.badge}" alt"${element.title}" >
+            <a href="${element.url}" target="_blank" class="btn btn-primary">See Course</a>
+          </div>`)
+      }, this); 
+    }
+  })
 });
