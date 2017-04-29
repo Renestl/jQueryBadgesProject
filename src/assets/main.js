16f3927741430162995e213a9da001cea3ec77fd
renestl <renestl@gmail.com>
2017-04-29 15:31:20 -0500

    changed to appendTo instead of append

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,12 +5,19 @@ $(function() {
     dataType: 'jsonp',
     success: function(response) {
       response.courses.completed.forEach(function(element) {
-        $('#badges').append(`
+        // $('#badges').append(`
+        //   <div class="course">
+        //     <h3>${element.title}</h3>
+        //     <img src="${element.badge}" alt"${element.title}" >
+        //     <a href="${element.url}" target="_blank" class="btn btn-primary">See Course</a>
+        //   </div>`)
+
+          $(`
           <div class="course">
             <h3>${element.title}</h3>
             <img src="${element.badge}" alt"${element.title}" >
             <a href="${element.url}" target="_blank" class="btn btn-primary">See Course</a>
-          </div>`)
+          </div>`).appendTo('#badges');
       }, this); 
     }
   })
